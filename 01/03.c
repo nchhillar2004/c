ef5055dd0af2a373d2c00be0056cd4e83f3720ce
Nishant Chhillar <nchhillar2004@gmail.com>
2026-03-24 23:02:46 +0530

    01

---
+++ 01/03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+    int c;
+
+    /*
+    c = getchar();
+    while (c != EOF) {
+        putchar(c);
+        c = getchar();
+    } */
+
+    while ((c = getchar()) != EOF)
+        putchar(c);
+}
